f433ae4cd007f92646a126b3b0022a8b89a914b5
Max <contact@mstoiber.com>
2017-01-08 19:44:21 +0100

    Add differentiation between GET and POST requests

--- index.js
+++ index.js
@@ -11,21 +11,29 @@ module.exports = async function (req, res) {
   if (pathname.length <= 1) {
     throw createError(400, 'Please include a path to a page.')
   }
-  if (req.method !== 'GET') {
-    throw createError(400, 'Please make a GET request.')
+  if (req.method !== 'GET' && req.method !== 'POST') {
+    throw createError(400, 'Please make a GET or a POST request.')
   }
   try {
     const views = parseInt(await db.get(pathname), 10)
     // Increment the views and send them back to client
     await db.put(pathname, views + 1)
-    send(res, 200, { views: views + 1 })
+    if (req.method === 'GET') {
+      send(res, 200, { views: views + 1 })
+    } else {
+      send(res, 200)
+    }
   } catch (err) {
     if (err.notFound) {
       // Initialise the page with one view
       await db.put(pathname, 1)
-      send(res, 200, { views: 1 })
+      if (req.method === 'GET') {
+        send(res, 200, { views: 1 })
+      } else {
+        send(res, 200)
+      }
     } else {
-      throw createError(500, 'Something went wrong, sorry about that.')
+      throw createError(500, 'Internal server error.')
     }
   }
 }
