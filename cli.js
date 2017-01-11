8718758d8b248b63bb30e9d4fb8abae534485383
Max <contact@mstoiber.com>
2017-01-11 18:44:31 +0100

    Add update notification to CLI

--- cli.js
+++ cli.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
 const { exec } = require('shelljs')
+const updateNotifier = require('update-notifier');
+const pkg = require('./package.json');
+
+updateNotifier({ pkg }).notify();
 
 exec('npm start -- ' + process.argv.join(' '), {
   async: true,
