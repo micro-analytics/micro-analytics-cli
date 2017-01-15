fa0ae5ca7254c655bd1caf88cbf6b4874e0b2c10
Max <contact@mstoiber.com>
2017-01-15 11:32:15 +0100

    Extract flat-file-db adapter into separate package

--- src/db.js
+++ src/db.js
@@ -1,22 +1,20 @@
 const promise = require('promise')
 
 let adapter
+const adapterName = `micro-analytics-adapter-${process.env.DB_ADAPTER || 'flat-file-db'}`
 
 const repeatCharacter = (char, n) => `${Array(n + 1).join(char)}`
 
-if (process.env.DB_ADAPTER) {
-  const adapterName = `micro-analytics-adapter-${process.env.DB_ADAPTER}`
-  try {
-    adapter = require(adapterName)
-  } catch (err) {
-    if (err.code === 'MODULE_NOT_FOUND') {
-      // Console.error a warning message, but normally exit the process to avoid printing ugly npm ERR lines and stack trace.
-      console.error(`\n${repeatCharacter(' ', 22)}⚠️ ERROR ⚠️\n${repeatCharacter('-', 55)}\nYou specified "${process.env.DB_ADAPTER}" as the DB_ADAPTER, but no package\ncalled "${adapterName}" was found.\n\nPlease make sure you spelled the name correctly and\nhave "npm install"ed the necessary adapter package!\n${repeatCharacter('-', 55)}\n`)
-      process.exit(0)
-    }
+try {
+  adapter = require(adapterName)
+} catch (err) {
+  if (err.code === 'MODULE_NOT_FOUND') {
+    // Console.error a warning message, but normally exit the process to avoid printing ugly npm ERR lines and stack trace.
+    console.error(`\n${repeatCharacter(' ', 22)}⚠️ ERROR ⚠️\n${repeatCharacter('-', 55)}\nYou specified "${process.env.DB_ADAPTER}" as the DB_ADAPTER, but no package\ncalled "${adapterName}" was found.\n\nPlease make sure you spelled the name correctly and\nhave "npm install"ed the necessary adapter package!\n${repeatCharacter('-', 55)}\n`)
+    process.exit(0)
+  } else {
+    throw err
   }
-} else {
-  adapter = require('./flat-file-adapter')
 }
 
 module.exports = {
