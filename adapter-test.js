fc1c817380933c49219b366385ea4a3d2dfa55ea
Max <contact@mstoiber.com>
2017-01-15 13:22:03 +0100

    Add workaround for Jest issue

--- adapter-test.js
+++ adapter-test.js
@@ -1,6 +1,30 @@
 #!/usr/bin/env node
-const { exec } = require('shelljs')
+const path = require('path')
+const { exec, mkdir, pushd, popd, rm } = require('shelljs')
+
+/**
+ * NOTE (@mxstbr): This is super hacky.
+ *
+ * Due to a bug upstream in jest-haste-map no tests that contain node_modules anywhere in their path
+ * can be run, not even explicitly. When this is installed the tests we want to run are in
+ * node_modules/micro-analytics-cli/tests/ — see the problem?
+ *
+ * We cuircumvent this bug by npm linking the adapter, cloning the whole micro-analytics project to
+ * /tmp/, installing the depencies, running the actual tests with the adapter and then deleting the temp folder again.
+ *
+ * I know, I know. I really want to ship this. (16th January, '17)
+ */
+const TMP_PATH = '/tmp/micro-analytics-adapter-test-suite'
 
 module.exports = (adapter) => {
-  exec('DB_ADAPTER=' + adapter + ' npm t', { cwd: __dirname })
+  exec('npm link')
+  exec('git clone https://github.com/mxstbr/micro-analytics ' + TMP_PATH, () => {
+    pushd(TMP_PATH)
+    exec('npm install', () => {
+      exec('DB_ADAPTER=' + adapter + ' npm t', () => {
+        popd()
+        rm('-rf', TMP_PATH)
+      })
+    })
+  })
 }
