8d9c8c22bff485d23f644e2bb4fffa53632d0280
Max <contact@mstoiber.com>
2017-01-17 18:12:07 +0100

    This might actually work now!

--- adapter-test.js
+++ adapter-test.js
@@ -1,9 +1,18 @@
 #!/usr/bin/env node
 const path = require('path')
+const ora = require('ora')
 const { exec } = require('shelljs')
 
 module.exports = (adapter) => {
-  exec('npm i', () => {
-    exec('DB_ADAPTER=' + adapter + ' npm t', { async: true, cwd: __dirname })
+  const spinner = ora('Installing dependencies').start()
+  exec('npm i', { cwd: __dirname, silent: true }, (exitCode, _, stderr) => {
+    const isError = exitCode > 0
+    if (isError) {
+      spinner.stopAndPersist('✖')
+      console.error(stderr)
+      return
+    }
+    spinner.stopAndPersist('✔')
+    const test = exec('DB_ADAPTER=' + adapter + ' npm t', { async: true, cwd: __dirname })
   })
 }
