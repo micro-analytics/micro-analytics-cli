178f813d012e616d29572c73f3ab3b98f51362df
Max <contact@mstoiber.com>
2017-01-17 18:54:43 +0100

    Remove unnecessary dependency

--- adapter-test.js
+++ adapter-test.js
@@ -1,18 +1,16 @@
 #!/usr/bin/env node
 const path = require('path')
-const ora = require('ora')
 const { exec } = require('shelljs')
 
 module.exports = (adapter) => {
-  const spinner = ora('Installing dependencies').start()
+  console.log('Installing dependencies...')
   exec('npm i', { cwd: __dirname, silent: true }, (exitCode, _, stderr) => {
     const isError = exitCode > 0
     if (isError) {
-      spinner.stopAndPersist('✖')
       console.error(stderr)
       return
     }
-    spinner.stopAndPersist('✔')
+    console.log('Dependencies installed!')
     const test = exec('TEST_DB_ADAPTER=' + path.join(process.cwd(), adapter) + ' npm run adapter-test', { async: true, cwd: __dirname })
   })
 }
