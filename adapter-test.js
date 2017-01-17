a180560186c3305f305faf7b74f029333fd1ecf0
Max <contact@mstoiber.com>
2017-01-17 17:43:27 +0100

    Go back to first simple version of adapter tests

--- adapter-test.js
+++ adapter-test.js
@@ -1,31 +1,7 @@
 #!/usr/bin/env node
 const path = require('path')
-const { exec, mkdir, pushd, popd, rm } = require('shelljs')
-
-/**
- * NOTE (@mxstbr): This is super hacky.
- *
- * Due to a bug upstream in jest-haste-map no tests that contain node_modules anywhere in their path
- * can be run, not even explicitly. When this is installed the tests we want to run are in
- * node_modules/micro-analytics-cli/tests/ — see the problem?
- *
- * We circumvent this bug by npm linking the adapter, cloning the whole micro-analytics project to
- * /tmp/, installing the dependencies, running the tests with the adapter and then deleting the
- * temp folder again.
- *
- * I know, I know. I really want to ship this though. (16th January, '17)
- */
-const TMP_PATH = '/tmp/micro-analytics-adapter-test-suite'
+const { exec } = require('shelljs')
 
 module.exports = (adapter) => {
-  exec('npm link')
-  exec('git clone https://github.com/mxstbr/micro-analytics ' + TMP_PATH, () => {
-    pushd(TMP_PATH)
-    exec('npm install', () => {
-      exec('DB_ADAPTER=' + adapter + ' npm t', () => {
-        popd()
-        rm('-rf', TMP_PATH)
-      })
-    })
-  })
+  exec('DB_ADAPTER=' + adapter + ' npm t', { async: true })
 }
