900d0e966ab5314829846917a8017ad18c93b567
Rolf Erik Lekang <me@rolflekang.com>
2017-03-24 21:51:44 +0100

    Initialize db with correct adapter

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ const micro = require('micro')
 const SSE = require('sse')
 const args = require('args');
 
-const handler = require('./handler')
-const sseHandler = require('./sse')
-
 const flags = args
   .option(['p', 'port'], 'Port to listen on', process.env.PORT || 3000, Number)
   .option(['H', 'host'], 'Host to listen on', '0.0.0.0')
   .option(['a', 'adapter'], 'Database adapter used', process.env.DB_ADAPTER || 'flat-file-db')
   .parse(process.argv, { name: 'micro-analytics' })
 
+const db = require('./db');
+db.initDbAdapter(flags.adapter)
+
+const handler = require('./handler')
 const server = micro(handler)
 const sse = new SSE(server)
 sse.on('connection', sseHandler)
--- src/sse.js
+++ src/sse.js
@@ -1,17 +1,11 @@
 const db = require('./db')
 
 module.exports = function handleSseConnection(connection) {
-  console.log('new connection')
   const subscription = db.subscribe((event) => {
-  console.log('new event', event)
-    connection.send({
-      event: 'new-event',
-      data: JSON.stringify(event),
-    })
+    connection.send({ event: 'new-event', data: JSON.stringify(event) })
   })
 
   connection.on('close', function () {
-    console.log('closing connection')
     subscription.unsubscribe()
   })
 }
