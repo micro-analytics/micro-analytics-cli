cdcf29f42260269ad8bae30312719bf48a9f7dc7
Rolf Erik Lekang <me@rolflekang.com>
2017-04-03 20:10:07 +0200

    Add tests with unit testsuite from micro-analytics-cli

---
+++ test.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const flatfile = require('flat-file-db')
+const promisify = require('then-flat-file-db')
+const test = require('micro-analytics-cli/adapter-tests/unit-tests')
+
+const db = promisify(flatfile.sync(process.env.DB_NAME || 'views.db'))
+
+
+test({
+  name: 'flat-file-db',
+  modulePath: path.resolve(__dirname, './index.js'),
+  beforeEach: () => {
+    return db.clear();
+  }
+})
