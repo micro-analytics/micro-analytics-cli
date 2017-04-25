ddcf3713b734264564fc04627e71050d79597e5b
Rolf Erik Lekang <me@rolflekang.com>
2017-04-25 07:52:25 +0200

    Revert "Autodeploy latest master to internal.micro-analytics.io"

--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-echo "Installing now-pipeline"
-npm install -g now-pipeline
-
-echo "Prune old instances"
-now-pipeline-prune
-
-echo "Deploy and alias new instance"
-now-pipeline --test 'curl --fail $NOW_URL/test?inc=false' --alias internal.micro-analytics.io
-
