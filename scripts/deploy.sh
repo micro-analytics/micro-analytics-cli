0f1bf15f926db544c880825958642fe5ef3d23b2
Rolf Erik Lekang <me@rolflekang.com>
2017-04-26 20:45:44 +0200

    Hide output from now-pipeline-prune

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@ echo "Installing now-pipeline"
 npm install -g MiniGod/now-pipeline#state-building
 
 echo "Prune old instances"
-now-pipeline-prune
+now-pipeline-prune > /dev/null
 
 echo "Deploy and alias new instance"
 now-pipeline --alias demo.micro-analytics.io --test "npm run test-now-deployment"
