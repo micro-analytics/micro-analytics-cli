57681736f33677d7cb6e06418cbd6207b0859fd7
Rolf Erik Lekang <me@rolflekang.com>
2017-06-24 09:31:37 +0200

    chore: Fix autodeployment of demo.micro-analytics.io

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,8 @@
+cd packages/micro-analytics-cli
+
+rm -rf node_modules
+npm install
+
 echo "Installing now-pipeline"
 npm install -g now-pipeline@latest
 
@@ -6,4 +11,5 @@ now-pipeline-prune > /dev/null
 
 echo "Deploy and alias new instance"
 now-pipeline --alias demo.micro-analytics.io --test "npm run test-now-deployment"
+#now-pipeline --alias demo.micro-analytics.io --test "true"
 
